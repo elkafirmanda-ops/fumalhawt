659c9b3c859451db8abdd474ad88c2a7b36d27d1
Elka Firmanda <elkafirmanda@gmail.com>
2020-11-09 18:27:04 +0700

    merge

--- apps/settings.py
+++ apps/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = 'hpr#7hb5b4akto#cwom!l8xa=fpnl4!=^^3=7-e90mt9yes_hz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
+ALLOWED_HOSTS = ['54.224.110.149','127.0.0.1']
 
 # Application definition
 
